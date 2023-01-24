abfcb5b6a54a30c632660bdc45a2e7dd3133d6ae
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2023-01-24 10:43:04 -0500

    setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const reactContentRoot = document.getElementById("root")
+
+const App = () =>{
+  return <p>Test</p>
+}
+
+ReactDOM.render(<App />, reactContentRoot)
