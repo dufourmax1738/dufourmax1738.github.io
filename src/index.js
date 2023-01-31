721bca8acad5b83d3185a46e2550b5279342bac9
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2023-01-31 12:04:01 -0500

    added base template and some personal info

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import About from './components/About'
+import {Navbar} from "react-bootstrap";
+import CustomNavbar from "./components/CustomNavbar";
 
 const reactContentRoot = document.getElementById("root")
 
-const App = () =>{
-  return <p>Test</p>
+const App = () => {
+  return (
+      <main className="text-gray-400 bg-gray-900 body-font">
+        <CustomNavbar/>
+        <About />
+      </main>
+  )
 }
 
 ReactDOM.render(<App />, reactContentRoot)
